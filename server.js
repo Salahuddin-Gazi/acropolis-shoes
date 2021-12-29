cf0f82f96f1c35cd62143072c21db7b036954722
Gazi Salahuddin <71849295+Salahuddin-Gazi@users.noreply.github.com>
2021-12-30 02:15:37 +0600

    Changed Port

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 // Connecting port
 app.listen(PORT, () => {
   console.log(`Server started on Port: ${PORT}`);
